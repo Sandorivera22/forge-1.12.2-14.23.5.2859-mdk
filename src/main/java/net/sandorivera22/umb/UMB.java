d0f5e5d7f219e38db39d52433885a7d559e297ae
Sandorivera22 <147453328+Sandorivera22@users.noreply.github.com>
2024-02-03 16:14:56 -0400

    Initial commit

---
+++ src/main/java/net/sandorivera22/umb/UMB.java
@@ -0,0 +1,31 @@
+package net.sandorivera22.umb;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = UMB.MOD_ID, name = UMB.NAME, version = UMB.VERSION)
+public class UMB
+{
+    public static final String MOD_ID = "umb";
+    public static final String NAME = "UMB";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
